4cb8b8305fe7c269363192bee8120c75f652c17e
Georgina Kimani <njokikimani001@gmail.com>
2023-08-12 11:24:08 -0700

    Betty checks

---
+++ betty_check.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * sub - Function that subtracts one value from another
+ * @a: the first value to use
+ * @b: the second value to subtract from a
+ * Return: the subtraction result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
