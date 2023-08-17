291163d68ef11fe0a3ca9c97d2226b19e4abd9f2
Georgina Kimani <njokikimani001@gmail.com>
2023-08-17 08:06:09 -0700

    Updated code

--- handle_path.c
+++ handle_path.c
@@ -8,37 +8,37 @@
 
 void handle_path(char **argv)
 {
-/*   Get the value of the PATH environment variable */
-char *path = _getenv("PATH");
-/* Tokenize the PATH variable using ':' as the delimiter */
-char *token = strtok(path, ":");
-/*   Store the maximum length for the command path */
-size_t max_length = 100;
-char *command_path = (char *)malloc(max_length);
+	/*   Get the value of the PATH environment variable */
+	char *path = _getenv("PATH");
+	/* Tokenize the PATH variable using ':' as the delimiter */
+	char *token = strtok(path, ":");
+	/*   Store the maximum length for the command path */
+	size_t max_length = 100;
+	char *command_path = (char *)malloc(max_length);
 
-if (command_path == NULL)
-{
-exit(EXIT_FAILURE);
-}
+	if (command_path == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
 
-/*    Iterate through each directory in the PATH */
-while(token != NULL)
-{
-/*  Construct the full path to the command executable */
-_strncpy(command_path, token, max_length - 1);
-command_path[max_length - 1] = '\0';
-_strcat(command_path, "/");
-_strcat(command_path, argv[0]);
+	/*    Iterate through each directory in the PATH */
+	while(token != NULL)
+	{
+		/*  Construct the full path to the command executable */
+		_strncpy(command_path, token, max_length - 1);
+		command_path[max_length - 1] = '\0';
+		_strcat(command_path, "/");
+		_strcat(command_path, argv[0]);
 
-/*  Check if the constructed path is an executable file and accessible */
-if(access(command_path, X_OK) == 0)
-{
-argv[0] = command_path;
-break; /* Stop searching further */
-}
+		/*  Check if the constructed path is an executable file and accessible */
+		if(access(command_path, X_OK) == 0)
+		{
+			argv[0] = strcpy(argv[0], command_path);
+			break; /* Stop searching further */
+		}
 
-/* Move on to the next directory in PATH */
-token = strtok(NULL, ":");
-}
-free(command_path);
+		/* Move on to the next directory in PATH */
+		token = strtok(NULL, ":");
+	}
+	free(command_path);
 }
--- prompt.c
+++ prompt.c
@@ -11,69 +11,71 @@
  */
 void prompt(char **av, char **env)
 {
-size_t n = 0;
-char *string = NULL;
-ssize_t num_char;
-int k, i, status;
-char *argv[MAXIMUM_COMMAND];
-pid_t child_pid;
+	size_t n = 0;
+	char *string = NULL;
+	ssize_t num_char;
+	int k, i, status;
+	char *argv[MAXIMUM_COMMAND];
+	pid_t child_pid;
 
-while(1)
-{
-if (isatty(STDIN_FILENO))
-printf("cisfun$ ");
-num_char = getline(&string, &n, stdin);
-if(num_char == -1)
-{
-free(string);
-exit(EXIT_FAILURE);
-}
-i = 0;
-while (string[i])
-{
-if (string[i] == '\n')
-{
-string[i] = 0;
-}
-i++;
-}
+	while(1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "cisfun$ ", 9);
+		num_char = getline(&string, &n, stdin);
+		if(num_char == -1)
+		{
+			free(string);
+			exit(EXIT_FAILURE);
+		}
+		i = 0;
+		while (string[i])
+		{
+			if (string[i] == '\n')
+			{
+				string[i] = 0;
+			}
+			i++;
+		}
 
-/* Check for exit command */
-if(_strcmp(string, "exit\n") == 0)
-{
-free(string);
-exit(EXIT_SUCCESS); 
-}
-k = 0;
-argv[k] = strtok(string, " ");
-while (argv[k] != NULL)
-argv[++k] = strtok(NULL, " ");
+		/* Check for exit command */
+		if(_strcmp(string, "exit\n") == 0)
+		{
+			free(string);
+			exit(EXIT_SUCCESS); 
+		}
+		k = 0;
+		argv[k] = strtok(string, " ");
+		while (argv[k] != NULL)
+		{
+			k++;
+			argv[k] = strtok(NULL, " ");
+		}
+		/*  Handle PATH using the new function */
+		handle_path(argv); /*  Call the new function to handle PATH */
 
-/*  Handle PATH using the new function */
-handle_path(argv); /*  Call the new function to handle PATH */
+		/*  If executable not found, skip fork and print error message */
+		if (argv[0] == NULL || access(argv[0], X_OK) != 0) {
+			printf("%s: command not found\n", argv[0]);
+			continue;
+		}
 
-/*  If executable not found, skip fork and print error message */
-if (argv[0] == NULL || access(argv[0], X_OK) != 0) {
-    printf("%s: command not found\n", argv[0]);
-    continue;
-}
-
-child_pid = fork();
-if (child_pid == -1)
-{
-free(string);
-exit(EXIT_FAILURE);
-}
-if (child_pid == 0)
-{
-if (execve(argv[0], argv, env) == -1)
-printf("%s: no such file or directory found\n", av[0]);
-free(string);
-exit(EXIT_FAILURE);
-}
-else
-{
-wait(&status);
-}
-}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			free(string);
+			exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, env) == -1)
+				printf("%s: no such file or directory found\n", av[0]);
+			free(string);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 }
