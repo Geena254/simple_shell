2e7567542768efbfe9fcb0f7155405db52e50be8
Georgina Kimani <njokikimani001@gmail.com>
2023-08-23 11:42:16 -0700

    Added comments

--- _exit.c
+++ _exit.c
@@ -1,11 +1,18 @@
 #include "shell.h"
 
+/**
+ * handl_exit - Function that terminates program
+ * when user types 'exit'
+ * @string: The string to check
+ * Return: Nothing.
+ */
+
 void handl_exit(char *string)
 {
 	/* Check for exit command */
 	if (_strcmp(string, "exit\n") == 0)
 	{
 		free(string);
-                exit(EXIT_SUCCESS);
+		exit(EXIT_SUCCESS);
 	}
 }
