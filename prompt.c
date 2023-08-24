7449b038540ac9a4c6e5e8e9ece80b5478fa4fd2
Georgina Kimani <njokikimani001@gmail.com>
2023-08-24 16:00:58 -0700

    Update

--- prompt.c
+++ prompt.c
@@ -9,44 +9,46 @@
  */
 void prompt(char **env)
 {
-	size_t n = 0;
-	char *string = NULL;
-	char *argv[] = {NULL, NULL};
-	int status;
-	pid_t child_pid;
-	ssize_t num_char;
-	char error_message[34];
+	int mode = isatty(STDIN_FILENO);
+        size_t n = 0;
+        char *string = NULL;
+        char *argv[] = {NULL, NULL};
+        int status;
+        pid_t child_pid;
+        ssize_t num_char;
+        char error_message[34];
 
-	while (1)
-	{
-		write(STDOUT_FILENO, "$ ", 2);
-		num_char =  getline(&string, &n, stdin);
-		if (num_char == -1)
-		{
-			free(string);
-			exit(EXIT_FAILURE);
-		}
-		remove_newline(string);
-		argv[0] = string;
-		child_pid = fork();
-		if (child_pid == -1)
-		{
-			free(string);
-			exit(EXIT_FAILURE);
-		}
-		if (child_pid == 0)
-		{
-			if (execve(argv[0], argv, env) == -1)
-			{
-				strcpy(error_message, "No such file or directory found\n");
-				write(STDOUT_FILENO, argv[0], _strlen(argv[0]));
-				write(STDOUT_FILENO, ": ", 2);
-				write(STDOUT_FILENO, error_message, _strlen(error_message));
-				free(string);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-			wait(&status);
-	}
+        while (1)
+        {
+		if (mode == 1) /* Print prompt in interactive mode */
+                	write(STDOUT_FILENO, "$ ", 2);
+                num_char =  getline(&string, &n, stdin);
+                if (num_char == -1)
+                {
+                        free(string);
+                        exit(EXIT_FAILURE);
+                }
+                remove_newline(string);
+                argv[0] = string;
+                child_pid = fork();
+                if (child_pid == -1)
+                {
+                        perror("Error: Fork");
+                        break;
+                }
+                if (child_pid == 0)
+                {
+                        if (execve(argv[0], argv, env) == -1)
+                        {
+                                strcpy(error_message, "No such file or directory found\n");
+                                write(STDOUT_FILENO, argv[0], _strlen(argv[0]));
+                                write(STDOUT_FILENO, ": ", 2);
+                                write(STDOUT_FILENO, error_message, _strlen(error_message));
+                                free(string);
+                                exit(1);
+                        }
+                }
+                else
+                        wait(&status);
+        }
 }
