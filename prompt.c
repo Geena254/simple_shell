d4571fee46be7d6f6b187e03e0acc9d7592bde66
nsohpeter <apiahpetertoch@gmail.com>
2023-08-25 15:06:11 -0700

    fix errorws

--- prompt.c
+++ prompt.c
@@ -7,31 +7,41 @@
  * This function creates a basic shell where the user can enter commands.
  * It displays a prompt "cisfun$" and waits for the user to input a command.
  */
+
 void prompt(char **env)
 {
 	size_t n = 0;
 	char *string = NULL;
-	char *argv[] = {NULL, NULL};
-	int status;
-	pid_t child_pid;
 	ssize_t num_char;
+	int i, status;
+	char *argv[] = {NULL, NULL};
+	pid_t child_pid;
 
-	while (1)
+	while(1)
 	{
-		write(STDOUT_FILENO, "$ ", 2);
-		if ((num_char = getline(&string, &n, stdin)) == -1)
+		if (isatty(STDIN_FILENO))
+			write(1, "cisfun$ ", 9);
+		num_char = getline(&string, &n, stdin);
+
+		if (num_char == -1)
 		{
 			free(string);
-			break;
+			exit(EXIT_FAILURE);
 		}
-		/* removing newline character */
-		remove_newline(string);
-
+		i = 0;
+		while (string[i])
+		{
+			if(string[i] == '\n')
+			{
+				string[i] = 0;
+			}
+			i++;
+		}
 		argv[0] = string;
 		child_pid = fork();
 		if (child_pid == -1)
 		{
-			perror("Fork");
+			free(string);
 			exit(EXIT_FAILURE);
 		}
 		if (child_pid == 0)
@@ -42,13 +52,13 @@ void prompt(char **env)
 				write(STDERR_FILENO, ": ", 2);
 				write(STDERR_FILENO, "No such file or directory found\n", 34);
 				free(string);
-				free(argv);
-				exit(1);
+				exit(EXIT_FAILURE);
 			}
 		}
 		else
+		{
 			wait(&status);
-		free(string);
-		free(argv);
+			free(string);
+		}
 	}
 }
